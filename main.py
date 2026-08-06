c645d20dfdcd2e2979d5751e9b85e0bb955c6cee
mosameer2098-ship-it <mosameer2098@gmail.com>
2026-08-07 01:02:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+from config import BOT_TOKEN
+from handlers import start, help_command, reset, chat
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("reset", reset))
+
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, chat)
+    )
+
+    print("✅ Nova AI Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
