bc279fe06d7f2ac730489367dab9349807a9fd08
mosameer2098-ship-it <mosameer2098@gmail.com>
2026-08-07 01:57:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from telegram.ext import (
+import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    from telegram.ext import (
     Application,
     CommandHandler,
     MessageHandler,
