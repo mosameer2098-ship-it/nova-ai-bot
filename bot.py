844f7019e17ee5256a9fe40d9441451f4751c6d1
mosameer2098-ship-it <mosameer2098@gmail.com>
2026-08-07 17:49:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
+from telegram.ext import Application
 
+from config import BOT_TOKEN
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+print("🤖 Bot Started Successfully...")
+
+app.run_polling()
