d44c607fa0c508fd20df0540e91922bc55880a56
mosameer2098-ship-it <mosameer2098@gmail.com>
2026-08-07 17:54:45 +0530

    Update message.py

--- handlers/message.py
+++ handlers/message.py
@@ -1,9 +1,13 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from utils.ai import generate_reply
+
 
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
 
-    await update.message.reply_text(
-        f"Received: {user_message}"
-    )
+    await update.message.chat.send_action("typing")
+
+    ai_reply = generate_reply(user_message)
+
+    await update.message.reply_text(ai_reply)
