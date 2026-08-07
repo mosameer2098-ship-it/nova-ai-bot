3f197144e409ebbb870541b19f81feac303b74bc
mosameer2098-ship-it <mosameer2098@gmail.com>
2026-08-07 17:51:05 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -1,1 +1,9 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "/start - Bot Start\n"
+        "/help - Help Menu\n"
+        "/clear - Chat History Clear (baad me add karenge)"
+    )
